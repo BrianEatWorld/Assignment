5380f170a9c18bf5568b6e284d0058b5493f32d1
Brian <BrianEatWorld@gmail.com>
2013-12-08 20:46:11 -0600

    Fixed issues with setup.py not packaging necessary files.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
-import os.path
+from os.path import join
+from os import walk
 from setuptools import setup,find_packages
 
 install_requires = [
@@ -11,21 +12,17 @@ test_requires = [
 'selenium',
 ]
 
-try:
-	setup(name='assignment',
-	      version='0.1',
-	      description='assignment for QA/S I/II',
-	      author='Brian Dorn',
-	      author_email='bdorn@rmn.com',
-	      packages=['assignment','tests',]
-	      )
+def get_package_data(dir):
+    root = 'assignment/'
+    paths = [p.replace(root, '') for p, _, f in walk(root + dir) if f]
+    extensions = ['html', 'js', 'coffee', 'css', 'less', 'xml', 'txt', 'png', 'jpg', 'jpeg', 'gif']
+    return [join(path, '*.' + ext) for path in paths for ext in extensions]
 
-except:
-	print("Couldn't find explicit packages, now searching")
-	setup(name='assignment',
-	      version='0.1',
-	      description='assignment for QA/S I/II',
-	      author='Brian Dorn',
-	      author_email='bdorn@rmn.com',
-	      packages= find_packages()
-	      )
+setup(name='Assignment',
+      version='0.1',
+      description='assignment for QA/S I/II',
+      author='Brian Dorn',
+      author_email='bdorn@rmn.com',
+      packages=find_packages(),
+      package_data={'assignment':get_package_data('semanticUIDocs')},
+      )
