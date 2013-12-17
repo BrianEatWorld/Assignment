996dc8c37b61d2901d69dc2ed150dc5732fb6ee4
Brian <BrianEatWorld@gmail.com>
2013-12-17 10:10:12 -0600

    work on tornado startup test

--- tests/test_tornado.py
+++ tests/test_tornado.py
@@ -2,46 +2,49 @@ import pytest
 import unittest
 import subprocess
 import requests
-import sys
 import time
+import logging
+import sys
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
-class semanticMain(unittest.TestCase):
+class tornadoTestMain(unittest.TestCase):
 
-	def setUp(self):
-		self.driver = webdriver.Firefox()
-		try:
-			self.basic_tornado = subprocess.Popen(["python","./assignment/basic.py"])
-			
-		except:
-			logging.info("error occured on start, killing")
-			self.basic_tornado.kill()
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        try:
+            self.basic_tornado = subprocess.Popen(["python", "./assignment/basic.py"])
 
+        except:
+            logging.info("error occured on start, killing")
+            self.basic_tornado.kill()
 
-	def test_wait_for_http(url,timeout,interval):
-		logging.info("Waiting for 2XX response from:",url)
-		for _ in range(int(timeout/interval)):
-			try:
-				assert str(requests.get(url).status_code)[0]=='2'
-				return
-			except:
-				sys.stdout.write('.')
-				sys.stdout.flush()
-				time.sleep(int(interval))
-		raise Exception('HTTP Timed Out on',url)
+    def test_wait_for_http(self):
+        url = "localhost:8888"
+        timeout = 2
+        interval = 1
+        logging.info("Waiting for 2XX response from:", url)
+        for _ in range(int(timeout/interval)):
+            try:
+                sys.stdout.write("successfully started")
+                assert str(requests.get(url).status_code)[0] == '2'
+                return
+            except:
+                sys.stdout.write('waiting')
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                time.sleep(int(interval))
 
-	def test_tornado_up(self):
-		baseurl = "localhost:8888"
-		driver = self.driver
-		driver.get(baseurl)
-		self.assertIn("Introduction", driver.title)
+    def test_tornado_up(self):
+        baseurl = "localhost:8888"
+        driver = self.driver
+        driver.get(baseurl)
+        self.assertIn("Introduction", driver.title)
 
-	def tearDown(self):
-		self.driver.close()
-		self.basic_tornado.kill()
+    def tearDown(self):
+        self.driver.close()
+        self.basic_tornado.kill()
 
 if __name__ == '__main__':
-    nose.main()
+    pytest.tornadoTestMain()
